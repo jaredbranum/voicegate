d2b36e132582cae551123a157e8b1df153faf5ec
Jared Branum <jared.branum@gmail.com>
2011-03-05 18:51:38 -0500

    xmpp handshake works up until tls encryption. working on that next

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
-var sys = require('sys');
-var net = require('net');
-var server = net.createServer(function (socket) {
+require.paths.unshift('.');
+
+var sys = require('sys')
+  , net = require('net')
+  , tls = require('tls')
+  , libxmljs = require("libxmljs")
+  , tlsserver = tls.createServer(function (socket){
+    
+  })
+  , tcpserver = net.createServer(function (socket) {
 
   // Every time someone connects, tell them hello and then close the connection.
   socket.addListener("connect", function () {
@@ -10,17 +17,52 @@ var server = net.createServer(function (socket) {
   });
   
   socket.addListener("data", function(data){
-    sys.puts('---');
+    sys.puts('---client---');
     sys.puts(data);
-    var reply = "<?xml version='1.0' ?><stream:stream xmlns='jabber:client' xmlns:stream='http://etherx.jabber.org/streams' id='c2s_123' from='example.com' version='1.0'>";
+    var reply;
+    if ( /<stream:stream/.test(data) ){ // setup xmpp handshake
+      reply = '<?xml version="1.0" ?>' +
+                '<stream:stream ' +
+                  'from="localhost" ' +
+                  'id="voicegate" ' +
+                  'xmlns="jabber:client" ' +
+                  'xmlns:stream="http://etherx.jabber.org/streams" ' +
+                  'version="1.0">';
+      socket.write(reply);
+      sys.puts('---server---');
+      sys.puts(reply);
+      reply = '<stream:features>' +
+                '<starttls xmlns="urn:ietf:params:xml:ns:xmpp-tls">' +
+                  '<required/>' +
+                '</starttls>' +
+                '<mechanisms xmlns="urn:ietf:params:xml:ns:xmpp-sasl">' +
+                  '<mechanism>DIGEST-MD5</mechanism>' +
+                  '<mechanism>PLAIN</mechanism>' +
+                '</mechanisms>' +
+              '</stream:features>';
+      socket.write(reply);
+      sys.puts('---server---');
+      sys.puts(reply);
+    } else if ( /<starttls\s+xmlns=('|").+('|")\s*\/>/.test(data) ) { // proceed tls encryption
+      reply = '<proceed xmlns="urn:ietf:params:xml:ns:xmpp-tls"/>';
+      socket.write(reply);
+      sys.puts('---server---');
+      sys.puts(reply);
+      tls
+    } else if ( /<\/stream:stream>/.test(data) ) {
+      reply = '</stream:stream>';
+      sys.puts('---server---');
+      sys.puts(reply);
+      //socket.end();
+    }
     socket.write("");
   });
   
 
 });
 
-// Fire up the server bound to port 7000 on localhost
-server.listen(5222, "localhost");
+// XMPP server bound to port 5222 on localhost
+tcpserver.listen(5222, "localhost");
 
 // Put a friendly message on the terminal
-console.log("TCP server listening on port 5222 at localhost.");
+console.log("VoiceGate XMPP gateway running on localhost port 5222");
