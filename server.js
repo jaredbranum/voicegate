838a25b53fa2a9f1626ae26bb3093f309641ef8b
Jared Branum <jared.branum@gmail.com>
2011-03-10 23:39:11 -0500

    now using libxmljs in xmpp-handler. changed xmpp-handler to be event-driven. code makes a lot more sense now but still no xmpp auth progress

--- server.js
+++ server.js
@@ -1,40 +1,35 @@
 require.paths.unshift('./lib');
 
-var sys       = require('sys')
+var host      = "localhost"
+  , sys       = require('sys')
   , net       = require('net')
   , tls       = require('tls')
   , fs        = require('fs')
   , xmpp      = require('xmpp-handler')
-  , libxmljs  = require("libxmljs")
-  , tlsserver = tls.createServer({
-    key:  fs.readFileSync('voicegate-key.pem'),
-    cert: fs.readFileSync('voicegate-cert.pem')
-  }, function (socket) {})
+  // , tlsserver = tls.createServer({ 
+  //   key:  fs.readFileSync('voicegate-key.pem'),
+  //   cert: fs.readFileSync('voicegate-cert.pem')
+  // }, function (socket) {})
   , tcpserver = net.createServer(function (socket) {
 
-  // Every time someone connects, tell them hello and then close the connection.
-  socket.addListener("connect", function () {
+  socket.on("connect", function () {
     sys.puts("Connection from " + socket.remoteAddress);
-    //socket.write("<?xml version='1.0' encoding='UTF-8' ?><success xmlns=\"urn:ietf:params:xml:ns:xmpp-sasl\" />");
-    //socket.end("Hello World\n");
   });
   
-  socket.addListener("data", function(data){
-    var reply = undefined;
-    if ( !xmpp.authenticated ){
-      reply = xmpp.startAuthentication(data);
-    }
-    
-    if ( reply !== undefined ){
-      socket.write(reply);
-    }
+  socket.on("data", function(data){
+    sys.puts('---client---\n' + data);
+    xmpp.process(data.toString());
+  });
+  
+  xmpp.on('reply', function(xmlresp){
+    sys.puts('---server---\n' + xmlresp);
+    socket.write(xmlresp);
   });
   
 
 });
 
 // XMPP server bound to port 5222 on localhost
-tcpserver.listen(5222, "localhost");
 
-// Put a friendly message on the terminal
-console.log("VoiceGate XMPP gateway running on localhost port 5222");
+tcpserver.listen(5222, host);
+console.log("VoiceGate XMPP gateway running on "+host+" port 5222");
