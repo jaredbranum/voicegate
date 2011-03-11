bcc7dcc8ad6ae231feece3ebce9ef2ec0dd316b4
Jared Branum <jared.branum@gmail.com>
2011-03-11 00:31:45 -0500

    getting username and password from xmpp client now. next step is to send them to google

--- server.js
+++ server.js
@@ -1,35 +1,27 @@
 require.paths.unshift('./lib');
 
-var host      = "localhost"
-  , sys       = require('sys')
-  , net       = require('net')
-  , tls       = require('tls')
-  , fs        = require('fs')
-  , xmpp      = require('xmpp-handler')
-  // , tlsserver = tls.createServer({ 
-  //   key:  fs.readFileSync('voicegate-key.pem'),
-  //   cert: fs.readFileSync('voicegate-cert.pem')
-  // }, function (socket) {})
-  , tcpserver = net.createServer(function (socket) {
-
-  socket.on("connect", function () {
-    sys.puts("Connection from " + socket.remoteAddress);
+var host        = "localhost"
+  , sys         = require('sys')
+  , net         = require('net')
+  , tls         = require('tls')
+  , fs          = require('fs')
+  , xmpp        = require('xmpp-handler')
+  , http        = require('https')
+  , httpsClient = http.createClient('80', 'https://www.google.com/voice')
+  , tcpServer   = net.createServer(function (socket){
+    socket.on("connect", function () {
+      sys.puts("Connection from " + socket.remoteAddress);
+    });  
+    socket.on("data", function(data){
+      sys.puts('---client---\n' + data);
+      xmpp.process(data.toString());
+    });  
+    xmpp.on('reply', function(xmlresp){
+      sys.puts('---server---\n' + xmlresp);
+      socket.write(xmlresp);
+    });
   });
-  
-  socket.on("data", function(data){
-    sys.puts('---client---\n' + data);
-    xmpp.process(data.toString());
-  });
-  
-  xmpp.on('reply', function(xmlresp){
-    sys.puts('---server---\n' + xmlresp);
-    socket.write(xmlresp);
-  });
-  
-
-});
 
 // XMPP server bound to port 5222 on localhost
-
-tcpserver.listen(5222, host);
+tcpServer.listen(5222, host);
 console.log("VoiceGate XMPP gateway running on "+host+" port 5222");
